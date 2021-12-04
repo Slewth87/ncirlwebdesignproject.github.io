e545442a5cb40323a3f04eddb026dd1209ea9740
Richard Duffy <rduffy@slack-corp.com>
2021-12-04 15:58:52 +0000

    Initial commit

---
+++ script/book.js
@@ -0,0 +1,38 @@
+function loadApp() {
+
+	var flipbook = $('.flipbook');
+
+ 	// Check if the CSS was already loaded
+	
+	if (flipbook.width()==0 || flipbook.height()==0) {
+		setTimeout(loadApp, 10);
+		return;
+	}
+
+	$('.flipbook .double').scissor();
+
+	// Create the flipbook
+
+	$('.flipbook').turn({
+			// Elevation
+
+			elevation: 50,
+			
+			// Enable gradients
+
+			gradients: true,
+			
+			// Auto center this flipbook
+
+			autoCenter: true
+
+	});
+}
+
+function highlight(el){
+    document.getElementById(el).style.maxWidth = "170px";
+}
+
+function restore(el){
+    document.getElementById(el).style.maxWidth = "150px";
+}
