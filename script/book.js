d66e43ecf702711d4fe5759e184d2954cef143e1
Richard Duffy <rduffy@slack-corp.com>
2021-12-08 13:17:27 +0000

    Storybook finished

--- script/book.js
+++ script/book.js
@@ -32,6 +32,7 @@ function loadApp() {
 
 var humptyPath = 0;
 var jackJillPath = 0;
+var missMuffetPath = 0;
 
 function start(story) {
 	document.getElementById(story+"-title").style.display = "none";
@@ -42,6 +43,7 @@ function start(story) {
 function go(story) {
 	console.log("Humpty: "+humptyPath);
 	console.log("JackJill: "+jackJillPath);
+	console.log("missMuffet: "+missMuffetPath);
 	if (story == "humpty") {
 		var lines = 5;
 		humptyPath++;
@@ -70,17 +72,39 @@ function go(story) {
 		if (jackJillPath > 2 && jackJillPath < 5) {
 			var img = "jackfall";
 			var img2 = "jill";
+			var split = "jillian";
 		};
 		if (jackJillPath == 5) {
 			var img = "jackfall";
 			var img2 = "jillfall";
+			var split = "jillian";
 		};
 		if (jackJillPath == 6) {
 			var img = "jackJill";
 		};
 	};
-	if (story == "jackJill" && jackJillPath > 2 && jackJillPath < 6) {
-		var stage = "<div class=\"col-12\"><div id=\"jillian\"><img class=\"story-element\" src=\"../images/"+img2+".png\" onclick=\"go('"+story+"')\"></div><div id="+story+(eval(story+"Path")+1)+"><img class=\"story-element\" src=\"../images/"+img+".png\" onclick=\"go('"+story+"')\"></div></div>"
+	if (story == "missMuffet") {
+		var lines = 6;
+		missMuffetPath++;
+		if (missMuffetPath == 1) {
+			var img = "muffetSat";
+		};
+		if (missMuffetPath > 1 && missMuffetPath < 5) {
+			var img = "muffetEat";
+		};
+		if (missMuffetPath > 2) {
+			var img2 = "spider";
+			var split = "spider"+missMuffetPath;
+		};
+		if (missMuffetPath == 5) {
+			var img = "spider";
+		};
+		if (missMuffetPath == 6) {
+			var img = "missMuffet";
+		};
+	};
+	if ( (story == "jackJill" && jackJillPath > 2 && jackJillPath < 6) || (story == "missMuffet" && missMuffetPath > 2 && missMuffetPath < 5) ) {
+		var stage = "<div class=\"col-12\"><div id=\""+split+"\"><img class=\"story-element\" src=\"../images/"+img2+".png\" onclick=\"go('"+story+"')\"></div><div id="+story+(eval(story+"Path")+1)+"><img class=\"story-element\" src=\"../images/"+img+".png\" onclick=\"go('"+story+"')\"></div></div>"
 	} else {
 		var stage = "<div class=\"col-12\" id="+story+(eval(story+"Path")+1)+"><img class=\"story-element\" src=\"../images/"+img+".png\" onclick=\"go('"+story+"')\"></div>";
 	}
@@ -97,6 +121,13 @@ function go(story) {
 			humptyPath = 0;
 		} else if (story == "jackJill") {
 			jackJillPath = 0;
+		} else if (story == "missMuffet") {
+			missMuffetPath = 0;
 		}
+	}
+	if (story == "missMuffet" && missMuffetPath == 5) {
+		document.getElementById("missMuffet-box2").style.visibility = "visible";
+	} else if (story == "missMuffet" && missMuffetPath != 5) {
+		document.getElementById("missMuffet-box2").style.visibility = "hidden";
 	}
 }
